7ddcb6791618180d08564f7b3a3da058399b5460
Jay Nash <jn202871@gmail.com>
2023-04-01 18:41:43 -0400

    Added Food Type To Spot

--- spot.java
+++ spot.java
@@ -1,16 +1,32 @@
 public class spot {
-    public int r;
-    public int g;
-    public int b;
+    private int r;
+    private int g;
+    private int b;
+    private int food; // 0:None, 1:Small, 2:Med, 3:Large
 
     public spot(double r, double g, double b){
         this.r = (int) Math.round(r);
         this.g = (int) Math.round(g);
         this.b = (int) Math.round(b);
+        double chance = Math.random();
+        if (chance > 0.95){
+            if( r >= b && r >= g)
+                food = 1;
+
+            else if (g >= r && g >= b)
+                food = 3;
+
+            else
+                food = 2;
+        } else food = 0;
     }
 
     public int getRGB(){
         int rgb = ((r&0x0ff)<<16)|((g&0x0ff)<<8)|(b&0x0ff);
         return rgb;
+    }
+
+    public int getFood(){
+        return food;
     }
 }
